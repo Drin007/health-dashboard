b69672de3a1efe42d6bcc40580a09dc46820a2d1
Drin007 <137424130+Drin007@users.noreply.github.com>
2026-04-16 07:09:23 +0530

    fix useEffect small error dependency issue

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import axios from "axios";
 function Dashboard() {
     const [deviceId, setDeviceId] = useState("");
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false );
 
     const fetchData = useCallback(async () => {
         if (!deviceId) return;
@@ -15,43 +15,44 @@ function Dashboard() {
             const res = await axios.get(
                 `https://health-backend-3qyn.onrender.com/api/health-data/${deviceId}`
             );
-            setData(res.data);
+
+            setData(res.data );
         } catch (err) {
-            console.log(err);
+            console.log( err );
         }
 
         setLoading(false);
-    }, [deviceId]);
+    }, [deviceId ]);
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        const interval = setInterval( ()=> {
             fetchData();
-        }, 5000);
+        }, 5000 );
 
-        return () => clearInterval(interval);
-    }, [fetchData]);
+        return ()=>clearInterval(interval);
+    }, [ fetchData ]);
 
     return (
-        <div style={{ border: "solid #ccc", fontWeight: "bold", fontSize: "20px", margin: "10px", padding: "15px", borderRadius: "8px", background: "#7b7b7b" }}>
+        <div style={{ border: "solid #ccc", fontWeight: "bold", fontSize: "20px", margin: "10px", padding: "15px", borderRadius: "8px", background: "#7b7b7b",  }}>
             <input
-                style={{ fontWeight: "bold", color: "grey", width: "300px", height: "40px", fontSize: "16px" }}
+                style={{ fontWeight: "bold", color: "grey", width: "300px", height: "40px", fontSize: "16px",  }}
                 placeholder="Enter deviceId"
                 value={deviceId}
-                onChange={(e) => setDeviceId(e.target.value)}
+                onChange={ ( e ) => setDeviceId( e.target.value )}
             />
 
-            <button onClick={fetchData} style={{ width: "300px", height: "40px", fontSize: "16px", fontWeight: "bold" }}>
+            <button onClick={fetchData} style={{ width: "300px", height: "40px", fontSize: "16px", fontWeight: "bold",  }}>
                 Fetch all the readings
             </button>
 
             {loading && <p>loading...</p>}
 
             <div style={{ backgroundColor: "#3a6079" }}>
-                {data.map((item, index) => (
-                    <div key={index} style={{ border: "solid #fbefef", margin: "10px", padding: "15px", borderRadius: "8px", background: "#f6f2f2" }}>
-                        <p>@🫀 Heart Rate: {item.heart_rate}</p>
-                        <p>@ SpO2: {item.spo2}</p>
-                        <p>@ Time: {item.timestamp}</p>
+                {data.map((i, index) => (
+                    <div key={index} style={{ border: "solid #fbefef", margin: "10px", padding: "15px", borderRadius: "8px", background: "#f6f2f2",  }}>
+                        <p>@🫀 Heart Rate: { i.heart_rate }</p>
+                        <p>@ SpO2: { i.spo2 }</p>
+                        <p>@ Time: { i.timestamp }</p>
                     </div>
                 ))}
             </div>
